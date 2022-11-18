0f0805685271f46a05778428d9fcd5d1e39b87ff
David Shorten <dpshorten@gmail.com>
2022-11-18 13:41:17 +1030

    various

---
+++ params.py
@@ -0,0 +1,5 @@
+class TLE_Lifetime_Analysis_Parameters:
+    TLE_files_directory = "processed_files/"
+    manoeuver_files_directory = "processed_files/"
+    #figs_output_directory = "figs/"
+    figs_output_directory = "/home/david/Documents/SatelliteTLEs/figs/"
